2e4b399a55c0224d4a133410e2274d5795bff15e
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2021-12-06 11:09:20 +0000

    Created Dual Control System
    Now you can assign 2 controllers to 2 balls by pressing the shoulder buttons on each respective controller

---
+++ Assets/Scripts/TestScripts/ControllerPickerTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerPickerTest : MonoBehaviour
+{
+    public bool p1, p2;
+    public GameObject ball1, ball2;
+    Gamepad player1, player2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1 == null && Gamepad.current.leftShoulder.ReadValue() == 1 && Gamepad.current.rightShoulder.ReadValue() == 1)
+        {
+            player1 = Gamepad.current;
+            p1 = true;
+            ball1.GetComponent<RollScript>().player = player1;
+        }
+        else if (player2 == null && Gamepad.current != player1 && Gamepad.current.leftShoulder.ReadValue() == 1 && Gamepad.current.rightShoulder.ReadValue() == 1)
+        {
+            player2 = Gamepad.current;
+            p2 = true;
+            ball2.GetComponent<RollScript>().player = player2;
+        }
+    }
+}
--- Assets/Scripts/TestScripts/RollScript.cs
+++ Assets/Scripts/TestScripts/RollScript.cs
@@ -7,15 +7,19 @@ public class RollScript : MonoBehaviour
 {
     public float Yaxis, Xaxis;
     public Rigidbody ball;
+    public Gamepad player;
 
     // Update is called once per frame
     void Update()
-    {
-        var gamepad = Gamepad.current;
-        Yaxis = gamepad.leftStick.y.ReadValue();
-        Xaxis = gamepad.leftStick.x.ReadValue();
+    {     
+        if(player != null)
+        {
+            Yaxis = player.leftStick.y.ReadValue();
+            Xaxis = player.leftStick.x.ReadValue();
 
-        ball.AddForce(new Vector3(1 * Xaxis, 0, 0));
-        ball.AddForce(new Vector3(0, 0, 1 * Yaxis));
+            ball.AddForce(new Vector3(1 * Xaxis, 0, 0));
+            ball.AddForce(new Vector3(0, 0, 1 * Yaxis));
+        }
+
     }
 }
