c53284d8543b8e9169ce785810bd6118a9413dd1
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2022-04-26 10:07:19 +0100

    Created Bumpers
    Bumpers will bounce the balls and players around!

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public string playerTag;
+    public string ballTag;
+    public float bounceForceCar = 100000;
+    public float bounceForceBall = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == playerTag)
+        {
+            Rigidbody otherRB = collision.rigidbody;
+
+            Debug.Log("Bumper Struck By Car!");
+
+            //Vector3 dir = collision.transform.position - transform.position;
+            //otherRB.AddForce(dir * bounceForce);
+
+            otherRB.AddExplosionForce(bounceForceCar, collision.contacts[0].point, 10);
+        }
+        else if(collision.transform.tag.Substring(0,4) == ballTag)
+        {
+            Rigidbody otherRB = collision.rigidbody;
+
+            Debug.Log("Bumper Struck By Ball!");
+
+            //Vector3 dir = collision.transform.position - transform.position;
+            //otherRB.AddForce(dir * bounceForce);
+
+            otherRB.AddExplosionForce(bounceForceBall, collision.contacts[0].point, 10);
+        }
+    }
+}
